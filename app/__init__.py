d0079039c79f8a89532551f129012eb9dd7f75bb
kunalbelkar43-cyber <kunalbelkar43@gmail.com>
2026-07-07 22:06:48 +0530

    Phase 3.4 - Configure Flask application

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+from config import Config
+from app.extensions import db, migrate, login_manager
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    login_manager.login_view = "auth.login"
+
+    @app.route("/")
+    def home():
+        return "<h1>Donor Management System</h1>"
+
+    return app
