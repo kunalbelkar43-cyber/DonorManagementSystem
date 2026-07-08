591942983c4f6b574d108abdc66d245e156971a4
kunalbelkar43-cyber <kunalbelkar43@gmail.com>
2026-07-08 11:25:08 +0530

    Phase 3.5 - Configure MySQL database

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    print(os.getenv("DATABASE_URL"))
